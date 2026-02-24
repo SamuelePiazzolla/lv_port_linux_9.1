7fe383fcae182159b11cab7f21c286ec05a25536
Samuele <samupiazzolla@gmail.com>
2026-02-24 12:30:57 +0100

    Test Commit

--- main.c
+++ main.c
@@ -59,6 +59,7 @@ static uint32_t get_time_ms(void)
 {
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
+    printf("prova");
     return (uint32_t)(ts.tv_sec * 1000 + ts.tv_nsec / 1000000);
 }
 
