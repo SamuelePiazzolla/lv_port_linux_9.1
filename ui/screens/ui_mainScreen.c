76e0268d1feed10cc996da15325e32e86c6f48bf
Samuele <spiazzolla@lifeelettronica.com>
2026-03-03 09:59:41 +0100

    Fix UI

--- ui/screens/ui_mainScreen.c
+++ ui/screens/ui_mainScreen.c
@@ -41,7 +41,7 @@ void ui_event_btnNfc(lv_event_t * e)
     lv_event_code_t event_code = lv_event_get_code(e);
 
     if(event_code == LV_EVENT_CLICKED) {
-        _ui_screen_change(&ui_connectivityScreen, LV_SCR_LOAD_ANIM_FADE_ON, 500, 0, &ui_connectivityScreen_screen_init);
+        _ui_screen_change(&ui_nfcScreen, LV_SCR_LOAD_ANIM_FADE_ON, 500, 0, &ui_nfcScreen_screen_init);
     }
 }
 
